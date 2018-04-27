78319efae553b31906469dcd4d10d2dff1d0d12d
Igor Vinnikov <i.vinnikov@godeltech.com>
2018-04-27 15:04:34 +0300

    included "else" block to show form.html

--- index.php
+++ index.php
@@ -1,9 +1,10 @@
 
 
 <?php
-ini_set("display_errors",1);
-ini_set("display_startup_errors",1);
-error_reporting(E_ALL);
+
+  ini_set('error_reporting', E_ALL);
+  ini_set('display_errors', 1);
+  ini_set('display_startup_errors', 1);;
 
 $tpl = fopen('template.tpl', 'r');
 $tpl = file('template.tpl');
@@ -17,13 +18,15 @@ if ($_GET) {
     $file = fopen("template.tpl","r");
     $text = file("template.tpl");
     if (!empty($_GET["username"]) && !empty($_GET["number"]) && !empty($_GET["monthnum"])){
+
+			$text_msg="";
       foreach ($tpl as $value) {
         $value = str_replace("%USERNAME%", $_GET["username"], $value);
         $value = str_replace("%NUMBER%", $_GET["number"], $value);
         $value = str_replace("%MONTHNUM%", $month_num, $value);
         $value = str_replace("%EXECDATE%", $exec_date, $value);
         $value = str_replace("%ENDDATE%", $end_date, $value);
-        echo $value . "<br/>";
+        $text_msg = $text_msg. $value . "<br/>";
       }
     }
    else {
@@ -57,4 +60,7 @@ if ($_GET) {
        echo "Variables are not defined!";
      }
   }
+	else {
+		 include('form.html');
+	}
 ?>
