65f4e520434fb1ba976df7e07548853341fe3236
Igor Vinnikov <i.vinnikov@godeltech.com>
2018-04-27 16:10:34 +0300

    added validation when running CLI

--- index.php
+++ index.php
@@ -34,30 +34,30 @@ if ($_GET) {
    }
 }
   elseif (isset($argv)) {
-    $user_name = $argv[1];
-    $_number = $argv[2];
-    $month_num = $argv[3];
+    if (!empty($argv[1]) && !empty($argv[2]) && !empty($argv[3])){
+      $user_name = $argv[1];
+      $_number = $argv[2];
+      $month_num = $argv[3];
 
-    $exec_date = date('d.m.Y H:i');
-     $end_date =date_create();
-     date_modify($end_date, $month_num."month");
-      $end_date = date_format($end_date, "d.m.y");
-      $file = fopen("template.tpl","r");
-      $text = file("template.tpl");
+      $exec_date = date('d.m.Y H:i');
+       $end_date =date_create();
+       date_modify($end_date, $month_num."month");
+        $end_date = date_format($end_date, "d.m.y");
+        $file = fopen("template.tpl","r");
+        $text = file("template.tpl");
 
-      $main_text="";
-      if (!empty($argv[1]) && !empty($argv[2]) && !empty($argv[3])){
-        foreach ($tpl as $value) {
-          $value = str_replace("%USERNAME%", $user_name, $value);
-          $value = str_replace("%NUMBER%", $_number, $value);
-          $value = str_replace("%MONTHNUM%", $month_num, $value);
-          $value = str_replace("%EXECDATE%", $exec_date, $value);
-          $value = str_replace("%ENDDATE%", $end_date, $value);
-          echo $main_text = $value;
-        }
+        $main_text="";
+      foreach ($tpl as $value) {
+        $value = str_replace("%USERNAME%", $user_name, $value);
+        $value = str_replace("%NUMBER%", $_number, $value);
+        $value = str_replace("%MONTHNUM%", $month_num, $value);
+        $value = str_replace("%EXECDATE%", $exec_date, $value);
+        $value = str_replace("%ENDDATE%", $end_date, $value);
+        echo $main_text = $value;
       }
+    }
      else {
-       echo "Variables are not defined!";
+       echo $main_text = "Variables are not defined!\n";
      }
   }
 	else {
